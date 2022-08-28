ba7086fa6737b4137d80cc0bf29e7805b744bb33
Uzzal Hosen <uzzalh.cse@gmail.com>
2022-08-28 17:26:41 +0600

    Package db migration schema

---
+++ database/migrations/2022_08_28_144744_create_package_features_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package_features', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('package_id')->constrained('packages')->cascadeOnDelete();
+            $table->string('key');
+            $table->text('value');
+            $table->boolean('is_enabled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package_features');
+    }
+};
